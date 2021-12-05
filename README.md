dab6df611ecf7df1cede45bd4a2de50ca2c565b5
Rileyc2022 <rileyc2022@headroyce.org>
2021-12-05 14:52:33 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# chat-example
+# Run with `yarn prod`
+This is the source code for a very simple chat example used for
+the [Getting Started](http://socket.io/get-started/chat/) guide
+of the Socket.IO website, but in Typescript.
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+const app = express();
+import { createServer } from "http";
+const http_server = createServer(app);
+import { Server } from "socket.io";
+const io = new Server(http_server);
+const port = process.env.PORT || 3000;
+
+// if not using a variable like req (request)
+// then making it an underscore will make the compiler happy
+// It's just a TS thing lol
+
+// app.get('/', (req, res) => {
+app.get("/", (_, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+    });
+});
+http_server.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
+// no types here cuz all the types are infered so far so that's nice
